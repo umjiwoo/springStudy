943af275abd5d42c6317db3525d6ddca6084c3fb
jwUm <87863185+umjiwoo@users.noreply.github.com>
2022-02-05 18:22:29 +0900

    스프링부트 프로그램이 시작되는 위치

---
+++ src/main/java/com/amber/book/springboot/Application.java
@@ -0,0 +1,12 @@
+package com.amber.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //스프링부트 자동 설정,Bean 읽기/생성 자동으로 생성해줌 !!프로젝트 최상단 클래스에 있어야함
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+        //SpringApplication.run->내장 WAS 실행시킴
+    }
+}
