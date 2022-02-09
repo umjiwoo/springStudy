1d8911bfa4e12585fcb26b5328af6c95aeeea025
jwUm <87863185+umjiwoo@users.noreply.github.com>
2022-02-09 23:39:50 +0900

    전체 게시글 조회하는 함수 구현

--- src/main/java/com/amber/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/amber/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.amber.book.springboot.service.posts;
 
 import com.amber.book.springboot.domain.posts.Posts;
 import com.amber.book.springboot.domain.posts.PostsRepository;
+import com.amber.book.springboot.web.dto.PostsListResponseDto;
 import com.amber.book.springboot.web.dto.PostsResponseDto;
 import com.amber.book.springboot.web.dto.PostsSaveRequestDto;
 import com.amber.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,19 @@ public class PostsService {
         Posts entity=postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) //readOnly=true;트랜잭션 범위는 유지하되 조회 기능만 남겨두어 조회 속도 개선
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+        //.map(PostsListResponseDto::new) == .map(posts->new PostsListResponseDto(posts))
+        //postsRepository 결과로 넘어온 Posts의 Stream을 map을통해 PostsListResponseDto로 변환한 후 리스트로 반환하도록 함
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts=postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        postsRepository.delete(posts);
     }
 }
