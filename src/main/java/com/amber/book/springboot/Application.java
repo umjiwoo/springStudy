89169373bc54ff4d6c0465afc5d17434b5dda81b
jwUm <87863185+umjiwoo@users.noreply.github.com>
2022-02-09 19:03:22 +0900

    데이터 생성/수정 시간 필드 추가 후 JPA Auditing 활성화

--- src/main/java/com/amber/book/springboot/Application.java
+++ src/main/java/com/amber/book/springboot/Application.java
@@ -2,7 +2,9 @@ package com.amber.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //JPA Auditing 활성화
 @SpringBootApplication //스프링부트 자동 설정,Bean 읽기/생성 자동으로 생성해줌 !!프로젝트 최상단 클래스에 있어야함
 public class Application {
     public static void main(String[] args){
