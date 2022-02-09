6b986ec20ed8988ce1dd80bb1444e78f3bda29ad
jwUm <87863185+umjiwoo@users.noreply.github.com>
2022-02-09 19:04:08 +0900

    데이터 생성/수정 시간 컬럼 자동으로 저장 테스트

--- src/test/java/com/amber/book/springboot/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/amber/book/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -46,5 +47,23 @@ public class PostsRepositoryTest {
         Posts posts=postsList.get(0); //그중 첫번째 데이터 선택
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void reg_BaseTimeEntity(){
+        LocalDateTime now=LocalDateTime.of(2022,2,9,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        List<Posts> postsList=postsRepository.findAll();
+        Posts posts=postsList.get(0);
+
+        System.out.println(">>>>> createdDate="+posts.getCreatedDate()+"modifiedDate="+posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
