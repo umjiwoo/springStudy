2a9b97c944cf9d3200f883556498b7056e997503
jwUm <87863185+umjiwoo@users.noreply.github.com>
2022-02-09 23:38:02 +0900

    전체 게시글 조회를 위한 메소드 생성

--- src/main/java/com/amber/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/amber/book/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.amber.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 //DB 접근할 접근자-인터페이스 생성 후 JpaRepository<Entity 클래스,PK 타입> 인터페이스를 상속받으면 기본적인 CRUD 메소드가 자동으로 생성됨
 //Entity 클래스와 기본 Repository 인터페이스는 같은 패키지 내에 있어야 함
-public interface PostsRepository extends JpaRepository<Posts,Long> {}
+public interface PostsRepository extends JpaRepository<Posts,Long> {
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")  //Spring Data Jpa에서 제공하지 않는 메소드는 직접 정의해서 사용해도 됨
+    List<Posts> findAllDesc();
+}
